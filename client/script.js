b12b30432e0cbd9d2c97373a5e6226f7b9673413
Arianna Choza <unachoza@gmail.com>
2019-02-23 22:31:59 -0500

    bottom nav bar with circle icons and clickevents

--- client/script.js
+++ client/script.js
@@ -1,7 +1,10 @@
 // alert('now')
-
+console.log('pring')
 const pageToInjury = () => {
     window.location.href='./identifyInjury.html'
     console.log('I was clicked')
 }
 
+const clickThis = () => {
+    console.log('I was clicked icon')
+}
