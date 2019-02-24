24939c8a252ae1cb3a64e23f4b286f57d92cd0d5
Arianna Choza <unachoza@gmail.com>
2019-02-24 00:53:18 -0500

    response card displaying

--- client/script.js
+++ client/script.js
@@ -8,3 +8,8 @@ const pageToInjury = () => {
 const clickThis = () => {
     console.log('I was clicked icon')
 }
+
+const nextCard = () => {
+    event.target.style.display = 'none'
+    console.log('did you disappear')
+}
